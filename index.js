74cc51a05cbb6d738c3a14abce07ae42cfc6cf20
garvit singhal <singhalgarvit63@gmail.com>
2025-06-19 20:58:53 +0530

    assignment done

---
+++ index.js
@@ -0,0 +1,19 @@
+let marks=prompt("enter marks between 0-100");
+
+if(marks <= 100 && marks >=0  ){
+    if(marks >=90 && marks <=100){
+        console.log("grade=A ,","Excellent");
+    }
+    else if(marks >=75 && marks <=89){
+        console.log("grade=B ,","Great job");
+    }
+    else if(marks >=60 && marks <=74){
+        console.log("grade=C ,","Good Effort");
+    }
+    else if(marks >=40 && marks <=59){
+        console.log("grade=D ,","You passed");
+    }
+    else if(marks >=0 && marks <=39){
+        console.log("grade=F ,","Better Luck Next Time");
+    }
+}
